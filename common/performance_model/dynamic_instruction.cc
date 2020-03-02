15b37599701c66c2767cc34f311e5abdc586dace
Dan <you@example.com>
2020-03-02 19:44:18 +0200

    uop_vp acess counter2

--- common/performance_model/dynamic_instruction.cc
+++ common/performance_model/dynamic_instruction.cc
@@ -44,7 +44,7 @@ SubsecondTime DynamicInstruction::getVPCost(Core *core, bool *p_is_mispredict, b
    const ComponentPeriod *period = core->getDvfsDomain();
    //std::cout << " test " << std::endl;
    if ( instruction->getbbhead() < 10 ) {
-	   std::cout << "DynamicInstruction::getVPCost_BBhead BBhead: " << std::dec << instruction->getbbhead() << std::endl;
+	   //std::cout << "DynamicInstruction::getVPCost_BBhead BBhead: " << std::dec << instruction->getbbhead() << std::endl;
 	   return static_cast<SubsecondTime>(*period) * 1;
    }
    //std::cout << "DynamicInstruction::getVPCost PC: " << std::hex << eip << " BBhead: " << instruction->getbbhead() << std::dec << " reg: " << vpinfo.regname << ": " << std::hex << vpinfo.value << std::endl;
